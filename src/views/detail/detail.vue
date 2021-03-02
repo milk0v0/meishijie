34cdf4e9eeac3f15e5afd5caadf1bb8c199368fb
milk <gongzhenyu40@qq.com>
2021-03-03 01:09:59 +0800

    views

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,9 +1,19 @@
 <template>
-	<div></div>
+	<div class="menu-detail">
+		<detail-header></detail-header>
+		<detail-content></detail-content>
+		<Comment></Comment>
+	</div>
 </template>
 
 <script>
-	export default {};
+	import DetailHeader from "./detail-header";
+	import DetailContent from "./detail-content";
+	import Comment from "./comment";
+	export default {
+		name: "detail",
+		components: { DetailHeader, DetailContent, Comment },
+	};
 </script>
 
 <style lang="scss" scoped>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,14 +2,14 @@
 	<div class="home">
 		<el-carousel :interval="5000" type="card" height="300px">
 			<el-carousel-item v-for="item in banners" :key="item._id">
-				<router-link :to="{ name: detail }">
+				<router-link :to="{ name: 'detail', query: { menuId: item._id } }">
 					<img :src="item.product_pic_url" width="100%" />
 				</router-link>
 			</el-carousel-item>
 		</el-carousel>
 		<div>
 			<h2>内容精选</h2>
-			<waterfall ref="waterfall">
+			<waterfall ref="waterfall" @view="handleLoadingMenu">
 				<menu-card :margin-left="13" :info="menuList"></menu-card>
 			</waterfall>
 		</div>
@@ -30,12 +30,29 @@
 			return {
 				banners: [],
 				menuList: [],
+				page: 1,
+				maxPage: 0,
 			};
 		},
+		methods: {
+			async handleLoadingMenu() {
+				this.page++;
+				if (this.page > this.maxPage) {
+					return (this.$refs.waterfall.isLoading = false);
+				}
+				const resMenus = await getMenus({ page: this.page });
+				this.menuList.push(...resMenus.data.list);
+				this.$refs.waterfall.isLoading = false;
+			},
+		},
 		async mounted() {
-			const res = await getBanner();
-			console.log(res.data.list);
-			this.banners = res.data.list;
+			const resBanner = await getBanner();
+			this.banners = resBanner.data.list;
+
+			const resMenus = await getMenus({ page: this.page });
+			this.maxPage = Math.ceil(resMenus.data.total / resMenus.data.page_size);
+			this.menuList.push(...resMenus.data.list);
+			this.$refs.waterfall.isLoading = false;
 		},
 	};
 </script>
