96011ae12acbf90f49f61442df363b4679512a26
milk <gongzhenyu40@qq.com>
2021-06-28 01:21:25 +0800

    init

--- src/components/menus.vue
+++ src/components/menus.vue
@@ -9,7 +9,7 @@
 			<router-link class="nav-link" :to="{ name: 'home' }">首页</router-link>
 		</el-menu-item>
 		<el-menu-item index="2">
-			<router-link class="nav-link" to=""
+			<router-link class="nav-link" :to="{ name: 'recipe' }"
 				>菜谱大全</router-link
 			>
 		</el-menu-item>
