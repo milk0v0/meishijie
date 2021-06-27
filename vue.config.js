ff0567cbb02a542b7d6b207ccfc3c60117f4151e
milk <gongzhenyu40@qq.com>
2021-06-27 21:09:47 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  publicPath: '/',
+  devServer: {
+    proxy: {
+      '/api': {  // http://localhost:8081/api/banner
+        target: 'http://127.0.0.1:7001',  // http://127.0.0.1:7001/banner
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
