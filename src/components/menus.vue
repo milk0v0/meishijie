ac3fb545ac04336282ac0eb60eba4edaf2565c8a
milk <gongzhenyu40@qq.com>
2021-07-28 23:00:28 +0800

    fix bug

--- src/components/menus.vue
+++ src/components/menus.vue
@@ -1,15 +1,15 @@
 <template>
 	<el-menu
-		default-active="1"
+		:default-active="$route.name"
 		class="el-menu-demo"
 		mode="horizontal"
 		:unique-opened="true"
 		:router="true"
 	>
-		<el-menu-item index="1" :route="{ name: 'home' }">
+		<el-menu-item index="home" :route="{ name: 'home' }">
 			<a>首页</a>
 		</el-menu-item>
-		<el-menu-item index="2" :route="{ name: 'recipe' }">
+		<el-menu-item index="recipe" :route="{ name: 'recipe' }">
 			<a>菜谱大全</a>
 		</el-menu-item>
 	</el-menu>
